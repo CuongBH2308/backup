26e610dde40f057b3a27610fbc647e66b5d4bcaa
lzprgmr <baiyanhuang@gmail.com>
2013-09-01 22:28:15 +0800

    discover method in a module by regexp

---
+++ learn/python/utility/lz.py
@@ -0,0 +1,15 @@
+import re
+import os
+
+def _discover(module, filter):
+	members = dir(module)
+	filter_members = [x for x in members if re.search(filter, x)]
+	return filter_members
+
+def dis(module, filter=r''):
+	print os.linesep.join(_discover(module, filter))
+	
+if __name__ == "__main__":
+	import string
+	dis(string, r'^c.*')
+	
